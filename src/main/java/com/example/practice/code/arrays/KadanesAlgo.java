56e0d60261ff03ef8735a66bbe37ea040ce10bdc
Parul Gupta <pgupta3009@gmail.com>
2026-01-16 21:21:16 +0530

    Update KadanesAlgo.java

--- src/main/java/com/example/practice/code/arrays/KadanesAlgo.java
+++ src/main/java/com/example/practice/code/arrays/KadanesAlgo.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
  */
 public class KadanesAlgo {
 
+    //Note that this solution fails for all-negatives array
     public long maximumSubarraySum(int[] a){
         long sum = 0;
         long max = Long.MIN_VALUE;
@@ -32,6 +33,19 @@ public class KadanesAlgo {
         return max;
     }
 
+    public long maximumSubarraySumBest(int[] nums){
+
+        int curSum = nums[0];
+        int maxSum = nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            curSum = Math.max(nums[i],nums[i]+curSum);
+            maxSum = Math.max(maxSum,curSum);
+        }
+        return maxSum;
+    }
+
+    //Note that this solution fails for all-negatives array
     public void printMaximumSubarraySum(int[] a){
         long sum = 0;
         long max = Long.MIN_VALUE;
@@ -64,6 +78,40 @@ public class KadanesAlgo {
             }
         }
     }
+
+
+    public int[] printMaximumSubarraySumBest(int[] nums) {
+
+        int maxSum = nums[0];
+        int currSum = nums[0];
+        int maxStart = 0, maxEnd = 0;
+        int currStart = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > currSum + nums[i]) {
+                currStart = i;  // Start new subarray
+            }
+            currSum = Math.max(nums[i], currSum + nums[i]);
+
+            if (currSum > maxSum) {
+                maxSum = currSum;
+                maxStart = currStart;
+                maxEnd = i;
+            }
+        }
+
+        // Print subarray
+        System.out.print("Max subarray: [");
+        for (int j = maxStart; j <= maxEnd; j++) {
+            System.out.print(nums[j]);
+            if (j < maxEnd) System.out.print(", ");
+        }
+        System.out.println("] with sum: " + maxSum);
+
+        return new int[]{maxStart, maxEnd, maxSum};
+    }
+
+
 
     public static void main(String[] args) {
         int[] a = {-2,-3,4,-1,-2,1,5,-3};
